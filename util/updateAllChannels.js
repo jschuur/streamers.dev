57c8922ed8b6575be4ea2863614a19f788bd3618
Joost Schuur <jschuur@jschuur.com>
2021-06-23 23:47:18 +0100

    Initial page load uses cached list of channels on CDN

--- util/updateAllChannels.js
+++ util/updateAllChannels.js
@@ -1,3 +1,4 @@
+import AWS, { ConnectContactLens } from 'aws-sdk';
 import consoleStamp from 'console-stamp';
 import minimist from 'minimist';
 import pluralize from 'pluralize';
@@ -10,19 +11,62 @@ const argv = minimist(process.argv.slice(2), {
 });
 
 import { updateAllChannelDetails, updateAllChannelStatuses } from '../lib/lib.js';
-import { disconnectDB } from '../lib/db.js';
+import { getChannels, disconnectDB } from '../lib/db.js';
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+async function saveToS3(data) {
+  const fileName =
+    process.env.NODE_ENV === 'production' ? 'live_channels.json' : 'live_channels_dev.json';
+
+  const params = {
+    Bucket: process.env.AWS_BUCKET,
+    Key: fileName,
+    Body: JSON.stringify(data, null, 2),
+    CacheControl: 'max-age=0',
+    ContentType: 'text/json',
+  };
+
+  console.log(process.env.AWS_BUCKET);
+  try {
+    const res = await s3.upload(params).promise();
+
+    console.log(
+      `File uploaded successfully: ${process.env.NEXT_PUBLIC_CACHED_CHANNELLIST_URL}/${fileName}`
+    );
+  } catch ({ message }) {
+    console.error(`Error uploading to S3: ${message}`);
+  }
+}
 
 (async () => {
   console.time('Time spent');
+
   console.log(`Running ${argv.fullDetails ? 'full details' : 'status'} update`);
+  console.log(`Mode: ${process.env.NODE_ENV}`);
 
   try {
     const { fullDetails, includePaused } = argv;
     let options = { updateAll: true, includePaused };
 
-    const channelCount = await (fullDetails
+    const channels = await (fullDetails
       ? updateAllChannelDetails(options)
       : updateAllChannelStatuses(options));
+
+    if (!fullDetails) {
+      console.log('Saving live channel JSON to S3...');
+      const liveChannels = await getChannels();
+
+      await saveToS3({
+        channels: liveChannels.filter((c) => c.isLive),
+        trackedChannelCount: channels.length,
+        createdAt: new Date(),
+      });
+    }
   } catch ({ message }) {
     console.error(`Problem updating all channels: ${message}`);
   }

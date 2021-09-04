6973472fc78cf8b7990578d4f33eb47adc7101bf
Joost Schuur <jschuur@jschuur.com>
2021-09-04 15:10:17 +0100

    Show flags as SVGs

--- components/Home/CountryFlags.js
+++ components/Home/CountryFlags.js
@@ -1,11 +1,11 @@
-import getUnicodeFlagIcon from 'country-flag-icons/unicode';
+import ReactCountryFlag from 'react-country-flag';
 
 export default function CountryFlags({ channel: { country, country2 } }) {
   const countries = [country, country2].filter(Boolean);
 
   return countries.map((country) => (
     <span key={country} className='m-1'>
-      {getUnicodeFlagIcon(country.toUpperCase())}
+      <ReactCountryFlag countryCode={country.toUpperCase()} svg />
     </span>
   ));
 }

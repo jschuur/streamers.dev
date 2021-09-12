57299927c0690973303b37cd13ca5d9b802e7e2f
Joost Schuur <jschuur@jschuur.com>
2021-09-12 23:16:09 +0100

    Stagger updates for channels that haven't streamed in a while

--- util/updateAllChannels.js
+++ util/updateAllChannels.js
@@ -7,7 +7,7 @@ import prettyMilliseconds from 'pretty-ms';
 import 'dotenv/config';
 
 const argv = minimist(process.argv.slice(2), {
-  boolean: ['fullDetails', 'includePaused'],
+  boolean: ['fullDetails', 'includePaused', 'localTest'],
   default: { includePaused: false },
 });
 
@@ -21,106 +21,120 @@ import {
 import { isProd } from '../lib/util';
 import logger from '../lib/logger';
 
+// Definitions to stagger checks for channels that haven't been live for a while,
+// to avoid hitting the Twitch API too often.
+const updateRanges = [
+  {
+    description: 'last online up to 7 days ago',
+    maxDaysSinceOnline: 7,
+    updatePercentage: 1,
+  },
+  {
+    description: 'last online 8-14 days ago',
+    minDaysSinceOnline: 7,
+    maxDaysSinceOnline: 14,
+    updatePercentage: 0.5,
+  },
+  {
+    description: 'last online over 15-28 days ago',
+    minDaysSinceOnline: 14,
+    maxDaysSinceOnline: 28,
+    updatePercentage: 0.25,
+  },
+  {
+    description: 'last online over 28 days ago',
+    minDaysSinceOnline: 28,
+    updatePercentage: 0.2,
+  },
+];
+
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
 });
 
-async function saveToS3(data) {
-  const fileName = isProd() ? 'live_channels.json' : 'live_channels_dev.json';
-
+async function saveToS3({ fileName, body }) {
   const params = {
     Bucket: process.env.AWS_BUCKET,
     Key: fileName,
-    Body: JSON.stringify(data, null, 2),
+    Body: JSON.stringify(body, null, 2),
     CacheControl: 'max-age=0',
     ContentType: 'text/json',
   };
 
-  try {
-    const res = await s3.upload(params).promise();
-
-    logger.info(
-      `File uploaded successfully: ${process.env.NEXT_PUBLIC_CACHED_CHANNELLIST_URL}/${fileName}`
-    );
-  } catch ({ message }) {
-    logger.error(`Error uploading to S3: ${message}`);
-  }
+  return s3.upload(params).promise();
 }
 
 async function saveLiveChannelCachedList() {
   logger.info('Saving live channel JSON to S3...');
 
+  const fileName = isProd() ? 'live_channels.json' : 'live_channels_dev.json';
   const channels = await getChannels({ isLive: true });
 
-  await saveToS3({
-    channels,
-    trackedChannelCount: await getTrackedChannelCount(),
-    distinctCountryCount: await getDistinctCountryCount(),
-    createdAt: new Date(),
+  try {
+    await saveToS3({
+      fileName,
+      body: {
+        channels,
+        trackedChannelCount: await getTrackedChannelCount(),
+        distinctCountryCount: await getDistinctCountryCount(),
+        createdAt: new Date(),
+      },
+    });
+
+    logger.info(
+      `...file uploaded successfully: ${process.env.NEXT_PUBLIC_CACHED_CHANNELLIST_URL}/${fileName}`
+    );
+  } catch ({ message }) {
+    logger.error(`...error uploading to S3: ${message}`);
+  }
+}
+
+async function updateRange({ range, now }) {
+  const { description, ...rangeOptions } = range;
+  const { includePaused } = argv;
+
+  logger.info(`Updating channels (${description})`);
+
+  return await updateAllChannelStatuses({
+    updateAll: true,
+    includePaused,
+    now,
+    ...rangeOptions,
   });
 }
 
 (async () => {
-  // Definitions for how often to check channels based on their lastOnline time
-  const updateRanges = [
-    {
-      maxDaysSinceOnline: 7,
-      minuteMark: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    },
-    {
-      minDaysSinceOnline: 7,
-      maxDaysSinceOnline: 14,
-      minuteMark: [0, 1, 4, 5],
-    },
-    {
-      minDaysSinceOnline: 14,
-      minuteMark: [2, 3],
-    },
-  ];
-  const { fullDetails, includePaused } = argv;
   const start = new Date();
-  let updateOptions = {
-    updateAll: true,
-    includePaused,
-  };
-
-  // Identify which channels need to be updated
-  const ranges = updateRanges.filter((r) => r.minuteMark.includes(getMinutes(start) % 10));
-
-  if (ranges.length === 0) {
-    logger.warn('No update ranges match this time of day (this should not happen)');
-    process.exit(1);
-  }
+  const { fullDetails, includePaused, localTest } = argv;
 
   logger.info(`Running ${argv.fullDetails ? 'full details' : 'status'} update`);
   logger.info(`Mode: ${process.env.NODE_ENV}`);
+  localTest && logger.info('Running in local test mode');
 
   try {
     // Development mode only runs updates every 10 mins, so it's fine to update everything
     // fullDetails one runs once an hour, so also update everything
-    if (process.env.NODE_ENV === 'production' && !fullDetails) {
-      for (const { minDaysSinceOnline, maxDaysSinceOnline } of ranges) {
-        logger.info(
-          `Updating channel range, min: ${minDaysSinceOnline || 'none'}, max: ${
-            maxDaysSinceOnline || 'none'
-          }`
-        );
+    if ((localTest || process.env.NODE_ENV === 'production') && !fullDetails) {
+      let count = 0;
 
-        await updateAllChannelStatuses({
-          ...updateOptions,
-          minDaysSinceOnline,
-          maxDaysSinceOnline,
-          now: start,
-        });
-      }
-    } else
+      for (const range of updateRanges)
+        count += (await updateRange({ range, now: start })).length || 0;
+
+      logger.info(`Total channels update: ${count}`);
+    } else {
+      const updateOptions = {
+        updateAll: true,
+        includePaused,
+      };
+
       await (fullDetails
         ? updateAllChannelDetails(updateOptions)
         : updateAllChannelStatuses(updateOptions));
+    }
 
-    if (!fullDetails) await saveLiveChannelCachedList();
+    await saveLiveChannelCachedList();
   } catch ({ message }) {
     logger.error(`Problem updating all channels: ${message}`);
   }

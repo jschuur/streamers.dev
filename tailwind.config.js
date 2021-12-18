4b80f4ed855c9161eea2971a7f49609f1a8402d1
Joost Schuur <jschuur@jschuur.com>
2021-12-18 07:32:26 +0000

    npm module updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,26 @@
 module.exports = {
-  purge: {
-    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-    // These are needed for dynamic class references in the Badge component
-    safelist: [
-      'bg-yellow-100',
-      'text-yellow-800',
-      'bg-red-100',
-      'text-red-800',
-      'bg-purple-100',
-      'text-purple-800',
-      'bg-green-100',
-      'text-green-800',
-      'bg-gray-100',
-      'text-gray-800',
-      'bg-blue-100',
-      'text-blue-800',
-    ],
-  },
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  // These are needed for dynamic class references in the Badge component
+  safelist: [
+    'bg-yellow-100',
+    'text-yellow-800',
+    'bg-red-100',
+    'text-red-800',
+    'bg-purple-100',
+    'text-purple-800',
+    'bg-green-100',
+    'text-green-800',
+    'bg-gray-100',
+    'text-gray-800',
+    'bg-blue-100',
+    'text-blue-800',
+  ],
   darkMode: 'class', // or 'media' or 'class'
-  mode: 'jit',
   theme: {
     extend: {
       fontFamily: {
         header: ['Fira Sans Condensed', 'sans-serif'],
       },
-    },
-  },
-  variants: {
-    extend: {
-      textColor: ['visited'],
     },
   },
   plugins: [require('tailwindcss-debug-screens')],

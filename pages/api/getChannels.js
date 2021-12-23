d6d74908cfdca8963059999b5020370f27c9de16
Joost Schuur <jschuur@jschuur.com>
2021-12-23 15:08:48 +0000

    WIP: Refactored Strean creation

--- pages/api/getChannels.js
+++ pages/api/getChannels.js
@@ -4,10 +4,16 @@ import { withSentry } from '@sentry/nextjs';
 import { getChannels, getTrackedChannelCount, getDistinctCountryCount } from '../../lib/db';
 import { updateChannelStatuses } from '../../lib/twitch';
 
+import { TWITCH_CHANNEL_FIELDS } from '../../lib/config';
+
 const handler = async (req, res) => {
   try {
     const { refresh } = req.query;
-    const channels = await getChannels({ isLive : true });
+
+    // Get channels and filter which data goes out via the API
+    const channels = map(await getChannels({ isLive: true }), (channel) =>
+      pick(channel, TWITCH_CHANNEL_FIELDS)
+    );
 
     // Add in updated status info in case channel data in the DB is outdated (or for live channels if configured)
     if (parseInt(refresh)) await updateChannelStatuses({ channels, updateAll: false });
---
+++ prisma/migrations/20211222141102_stream_history/migration.sql
@@ -0,0 +1,28 @@
+-- AlterTable
+ALTER TABLE "Channel" ADD COLUMN     "latestStreamTwitchId" TEXT;
+
+-- CreateTable
+CREATE TABLE "Stream" (
+    "id" SERIAL NOT NULL,
+    "channelId" INTEGER NOT NULL,
+    "twitchStreamId" TEXT NOT NULL,
+    "isCoding" BOOLEAN NOT NULL DEFAULT false,
+    "isLive" BOOLEAN NOT NULL DEFAULT false,
+    "title" TEXT,
+    "viewers" INTEGER,
+    "peakViewers" INTEGER,
+    "language" TEXT,
+    "gameName" TEXT,
+    "tags" TEXT[],
+    "twitchTags" TEXT[],
+    "startedAt" TIMESTAMP(3) NOT NULL,
+    "lastOnline" TIMESTAMP(3),
+
+    CONSTRAINT "Stream_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Stream_twitchStreamId_key" ON "Stream"("twitchStreamId");
+
+-- AddForeignKey
+ALTER TABLE "Stream" ADD CONSTRAINT "Stream_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

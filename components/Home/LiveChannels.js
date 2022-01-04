34d5517722dd5561acadc5135e3f17d88ab2607b
Joost Schuur <jschuur@jschuur.com>
2022-01-04 11:56:32 +0000

    Hot fix: Live channel loading animation back

--- components/Home/LiveChannels.js
+++ components/Home/LiveChannels.js
@@ -1,4 +1,7 @@
+import { useTheme } from 'next-themes';
+
 import ChannelListEntry from './ChannelListEntry';
+import Loader from '../Layout/Loader';
 
 import useChannelList from '../../hooks/useChannelList';
 import useFilterNav from '../../hooks/useFilterNav';
@@ -6,10 +9,16 @@ import useFilterNav from '../../hooks/useFilterNav';
 export default function LiveChannels({ initialChannels }) {
   const { visibleChannels, error } = useChannelList({ initialChannels });
   const filterNav = useFilterNav();
+  const { theme } = useTheme();
 
   if (error) return <div className='py-2'>Error: {error} </div>;
 
-  if (!visibleChannels) return null;
+  if (!visibleChannels)
+    return (
+      <div className='bg-gray-100 dark:bg-gray-700'>
+        <Loader message='Loading live channel list...' theme={theme} />
+      </div>
+    );
 
   if (!visibleChannels?.length)
     return (
